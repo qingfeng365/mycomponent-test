ef876ef43e0becb276db31f391d463ee969c3e47
qingfengwuyou <h3658686@126.com>
2017-08-15 17:20:27 +0800

    no message

--- src/app/content-muti-demo-child/content-muti-demo-child.component.ts
+++ src/app/content-muti-demo-child/content-muti-demo-child.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-content-muti-demo-child',
   templateUrl: './content-muti-demo-child.component.html',
   styleUrls: ['./content-muti-demo-child.component.css']
 })
-export class ContentMutiDemoChildComponent implements OnInit {
+export class ContentMutiDemoChildComponent implements OnInit,
+OnDestroy {
 
   constructor() { }
 
   ngOnInit() {
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy...');
   }
 
 }
